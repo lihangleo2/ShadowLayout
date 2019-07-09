73ade4b2b294f7b11261389f388e2bc9b5626922
unknown <lihangleo2@163.com>
2019-07-09 15:36:07 +0800

    7.9首次提交

---
+++ shadowLibrary/src/main/java/com/lihang/ShadowLayout.java
@@ -0,0 +1,209 @@
+package com.lihang;
+
+/**
+ * Created by leo
+ * on 2019/7/9.
+ * 阴影控件
+ */
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+
+public class ShadowLayout extends FrameLayout {
+    private int mShadowColor;
+    private float mShadowLimit;
+    private float mCornerRadius;
+    private float mDx;
+    private float mDy;
+    private boolean leftShow;
+    private boolean rightShow;
+    private boolean topShow;
+    private boolean bottomShow;
+
+
+    private boolean mInvalidateShadowOnSizeChanged = true;
+    private boolean mForceInvalidateShadow = false;
+
+    public ShadowLayout(Context context) {
+        super(context);
+        initView(context, null);
+    }
+
+    public ShadowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context, attrs);
+    }
+
+    public ShadowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context, attrs);
+    }
+
+    @Override
+    protected int getSuggestedMinimumWidth() {
+        return 0;
+    }
+
+    @Override
+    protected int getSuggestedMinimumHeight() {
+        return 0;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (w > 0 && h > 0 && (getBackground() == null || mInvalidateShadowOnSizeChanged || mForceInvalidateShadow)) {
+            mForceInvalidateShadow = false;
+            setBackgroundCompat(w, h);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        if (mForceInvalidateShadow) {
+            mForceInvalidateShadow = false;
+            setBackgroundCompat(right - left, bottom - top);
+        }
+    }
+
+    public void setInvalidateShadowOnSizeChanged(boolean invalidateShadowOnSizeChanged) {
+        mInvalidateShadowOnSizeChanged = invalidateShadowOnSizeChanged;
+    }
+
+    public void invalidateShadow() {
+        mForceInvalidateShadow = true;
+        requestLayout();
+        invalidate();
+    }
+
+    private void initView(Context context, AttributeSet attrs) {
+        initAttributes(context, attrs);
+
+        int xPadding = (int) (mShadowLimit + Math.abs(mDx));
+        int yPadding = (int) (mShadowLimit + Math.abs(mDy));
+        int left;
+        int right;
+        int top;
+        int bottom;
+        if (leftShow) {
+            left = xPadding;
+        } else {
+            left = 0;
+        }
+
+        if (topShow) {
+            top = yPadding;
+        } else {
+            top = 0;
+        }
+
+
+        if (rightShow) {
+            right = xPadding;
+        } else {
+            right = 0;
+        }
+
+        if (bottomShow) {
+            bottom = yPadding;
+        } else {
+            bottom = 0;
+        }
+
+        setPadding(left, top, right, bottom);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void setBackgroundCompat(int w, int h) {
+        Bitmap bitmap = createShadowBitmap(w, h, mCornerRadius, mShadowLimit, mDx, mDy, mShadowColor, Color.TRANSPARENT);
+        BitmapDrawable drawable = new BitmapDrawable(getResources(), bitmap);
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN) {
+            setBackgroundDrawable(drawable);
+        } else {
+            setBackground(drawable);
+        }
+    }
+
+
+    private void initAttributes(Context context, AttributeSet attrs) {
+        TypedArray attr = getTypedArray(context, attrs, R.styleable.ShadowLayout);
+        if (attr == null) {
+            return;
+        }
+
+        try {
+            //默认是显示
+            leftShow = attr.getBoolean(R.styleable.ShadowLayout_hl_leftShow, true);
+            rightShow = attr.getBoolean(R.styleable.ShadowLayout_hl_rightShow, true);
+            bottomShow = attr.getBoolean(R.styleable.ShadowLayout_hl_bottomShow, true);
+            topShow = attr.getBoolean(R.styleable.ShadowLayout_hl_topShow, true);
+
+            mCornerRadius = attr.getDimension(R.styleable.ShadowLayout_hl_cornerRadius, getResources().getDimension(R.dimen.dp_5));
+            mShadowLimit = attr.getDimension(R.styleable.ShadowLayout_hl_shadowLimit, getResources().getDimension(R.dimen.dp_5));
+            mDx = attr.getDimension(R.styleable.ShadowLayout_hl_dx, 0);
+            mDy = attr.getDimension(R.styleable.ShadowLayout_hl_dy, 0);
+            mShadowColor = attr.getColor(R.styleable.ShadowLayout_hl_shadowColor, getResources().getColor(R.color.default_shadow_color));
+        } finally {
+            attr.recycle();
+        }
+    }
+
+    private TypedArray getTypedArray(Context context, AttributeSet attributeSet, int[] attr) {
+        return context.obtainStyledAttributes(attributeSet, attr, 0, 0);
+    }
+
+    private Bitmap createShadowBitmap(int shadowWidth, int shadowHeight, float cornerRadius, float shadowRadius,
+                                      float dx, float dy, int shadowColor, int fillColor) {
+
+        Bitmap output = Bitmap.createBitmap(shadowWidth, shadowHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        RectF shadowRect = new RectF(
+                shadowRadius,
+                shadowRadius,
+                shadowWidth - shadowRadius,
+                shadowHeight - shadowRadius);
+
+        if (dy > 0) {
+            shadowRect.top += dy;
+            shadowRect.bottom -= dy;
+        } else if (dy < 0) {
+            shadowRect.top += Math.abs(dy);
+            shadowRect.bottom -= Math.abs(dy);
+        }
+
+        if (dx > 0) {
+            shadowRect.left += dx;
+            shadowRect.right -= dx;
+        } else if (dx < 0) {
+            shadowRect.left += Math.abs(dx);
+            shadowRect.right -= Math.abs(dx);
+        }
+
+        Paint shadowPaint = new Paint();
+        shadowPaint.setAntiAlias(true);
+        shadowPaint.setColor(fillColor);
+        shadowPaint.setStyle(Paint.Style.FILL);
+
+        if (!isInEditMode()) {
+            shadowPaint.setShadowLayer(shadowRadius, dx, dy, shadowColor);
+        }
+
+        canvas.drawRoundRect(shadowRect, cornerRadius, cornerRadius, shadowPaint);
+
+        return output;
+    }
+
+}
+
