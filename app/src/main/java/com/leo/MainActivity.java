931bad7a2a27c367cd8fd6aa1f6b9308f7e700e7
leo <lihangleo2@gmail.com>
2019-08-08 19:10:51 +0800

    ShadowLayout 2.0震撼上线

--- app/src/main/java/com/leo/MainActivity.java
+++ app/src/main/java/com/leo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.leo;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.buttonPanel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StarShowActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/leo/StarShowActivity.java
@@ -0,0 +1,238 @@
+package com.leo;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+public class StarShowActivity extends AppCompatActivity implements View.OnClickListener {
+    private com.lihang.ShadowLayout ShadowLayout;
+    private SeekBar skbar_x;
+    private SeekBar skbar_y;
+    private SeekBar skbar_limit;
+    private SeekBar skbar_corner;
+    private int alpha;
+    private SeekBar skbar_alpha;
+    private int red;
+    private SeekBar skbar_red;
+    private int green;
+    private SeekBar skbar_green;
+    private int blue;
+    private SeekBar skbar_blue;
+    private ImageView tab_topShow;
+    private ImageView tab_bottomShow;
+    private ImageView tab_rightShow;
+    private ImageView tab_leftShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starshow);
+        ShadowLayout = findViewById(R.id.ShadowLayout);
+        skbar_x = findViewById(R.id.skbar_x);
+        skbar_y = findViewById(R.id.skbar_y);
+        skbar_limit = findViewById(R.id.skbar_limit);
+        skbar_corner = findViewById(R.id.skbar_corner);
+        skbar_alpha = findViewById(R.id.skbar_alpha);
+        skbar_red = findViewById(R.id.skbar_red);
+        skbar_green = findViewById(R.id.skbar_green);
+        skbar_blue = findViewById(R.id.skbar_blue);
+        tab_topShow = findViewById(R.id.tab_topShow);
+        tab_topShow.setOnClickListener(this);
+        tab_bottomShow = findViewById(R.id.tab_bottomShow);
+        tab_bottomShow.setOnClickListener(this);
+        tab_rightShow = findViewById(R.id.tab_rightShow);
+        tab_rightShow.setOnClickListener(this);
+        tab_leftShow = findViewById(R.id.tab_leftShow);
+        tab_leftShow.setOnClickListener(this);
+
+
+        skbar_corner.setMax((int) (ShadowLayout.getmCornerRadius() * 3));
+        skbar_corner.setProgress((int) ShadowLayout.getmCornerRadius());
+        skbar_corner.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ShadowLayout.setmCornerRadius(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        skbar_limit.setMax((int) (ShadowLayout.getmShadowLimit() * 3));
+        skbar_limit.setProgress((int) ShadowLayout.getmShadowLimit());
+        skbar_limit.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ShadowLayout.setmShadowLimit(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        skbar_x.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ShadowLayout.setMDx(progress - 100);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        skbar_y.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                ShadowLayout.setMDy(progress - 100);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        skbar_alpha.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                alpha = progress;
+                ShadowLayout.setmShadowColor(Color.argb(alpha, red, green, blue));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        skbar_red.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                red = progress;
+                ShadowLayout.setmShadowColor(Color.argb(alpha, red, green, blue));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        skbar_green.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                green = progress;
+                ShadowLayout.setmShadowColor(Color.argb(alpha, red, green, blue));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        skbar_blue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                blue = progress;
+                ShadowLayout.setmShadowColor(Color.argb(alpha, red, green, blue));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tab_topShow:
+                ShadowLayout.setTopShow(!select(tab_topShow));
+                break;
+
+            case R.id.tab_bottomShow:
+                ShadowLayout.setBottomShow(!select(tab_bottomShow));
+                break;
+
+            case R.id.tab_leftShow:
+                ShadowLayout.setLeftShow(!select(tab_leftShow));
+                break;
+
+            case R.id.tab_rightShow:
+                ShadowLayout.setRightShow(!select(tab_rightShow));
+                break;
+        }
+    }
+
+
+    public boolean select(ImageView imageView) {
+        if (imageView.isSelected()) {
+            imageView.setSelected(false);
+            return false;
+        } else {
+            imageView.setSelected(true);
+            return true;
+        }
+    }
+}
